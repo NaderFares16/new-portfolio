ff074071a5a554ee6cc10bd7fc5665b1a08566c0
Nader Fares <naderalifares16@hotmail.com>
2024-01-15 14:37:36 -0300

    skills section complete

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ function scrollToSection(banner) {
       section.scrollIntoView({ behavior: 'smooth' });
   }
 }
+
+function scrollToSection(about) {
+  var section = document.getElementById(about);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToSection(skills) {
+  var section = document.getElementById(skills);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToSection(projects) {
+  var section = document.getElementById(projects);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
+
+function scrollToSection(getintouch) {
+  var section = document.getElementById(getintouch);
+  if (section) {
+      section.scrollIntoView({ behavior: 'smooth' });
+  }
+}
